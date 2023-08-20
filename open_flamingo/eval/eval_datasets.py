54c14426dfef41303e102e0124d3cba8a6ebbcd6
Alexander Loftus <alex@creyonbio.com>
2023-08-20 13:59:07 -0700

    start on VisDial Dataset class

--- open_flamingo/eval/eval_datasets.py
+++ open_flamingo/eval/eval_datasets.py
@@ -7,6 +7,60 @@ from torchvision.datasets import ImageFolder
 
 from open_flamingo.eval.classification_utils import IMAGENET_CLASSNAMES
 
+class EvalDialDataset(Dataset):
+    def __init__(
+        self,
+        image_train_dir_path,
+        annotations_path,
+        is_train,
+        dataset_name,
+        image_val_dir_path=None,
+    ):
+        self.image_train_dir_path = image_train_dir_path
+        self.image_val_dir_path = image_val_dir_path
+        self.annotations = []
+        self.is_train = is_train
+        self.dataset_name = dataset_name
+
+        full_annotations = json.load(open(annotations_path))["images"]
+
+        for i in range(len(full_annotations)):
+            if self.is_train and full_annotations[i]["split"] != "train":
+                continue
+            elif not self.is_train and full_annotations[i]["split"] != "test":
+                continue
+
+            self.annotations.append(full_annotations[i])
+
+    def __len__(self):
+        return len(self.annotations)
+
+    def __getitem__(self, idx):
+        if self.dataset_name == "coco":
+            image = Image.open(
+                os.path.join(
+                    self.image_train_dir_path, self.annotations[idx]["filename"]
+                )
+                if self.annotations[idx]["filepath"] == "train2014"
+                else os.path.join(
+                    self.image_val_dir_path, self.annotations[idx]["filename"]
+                )
+            )
+        elif self.dataset_name == "flickr":
+            image = Image.open(
+                os.path.join(
+                    self.image_train_dir_path, self.annotations[idx]["filename"]
+                )
+            )
+        image.load()
+        caption = self.annotations[idx]["sentences"][0]["raw"]
+        return {
+            "image": image,
+            "caption": caption,
+            "image_id": self.annotations[idx]["cocoid"]
+            if self.dataset_name == "coco"
+            else self.annotations[idx]["filename"].split(".")[0],
+        }
 
 class CaptionDataset(Dataset):
     def __init__(
@@ -155,3 +209,20 @@ class HatefulMemesDataset(Dataset):
             "class_name": "yes" if annotation["label"] == 1 else "no",
             "class_id": annotation["label"],
         }
+
+class VisDialDataset(Dataset):
+    def __init__(self, image_dir_path, annotations_path):
+        self.image_dir_path = image_dir_path
+        self.annotions_path = annotations_path
+        with open(self.annotions_path, "r") as f:
+            json = json.load(f)
+            self.data = json["data"]
+        
+    def __len__(self):
+        return len(self.data['questions'])
+
+    def __getitem__(self, idx):
+        """
+        idx : question ID.
+        """
+        question: str = self.data['questions'][idx]
